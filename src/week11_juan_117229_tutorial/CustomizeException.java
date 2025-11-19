64a986190d0b9cf50e8fcc8e321643e961bed7a0
Juan <juanlowandy@gmail.com>
2025-11-19 20:47:45 +0700

    Custom Exceptions

---
+++ src/week11_juan_117229_tutorial/CustomizeException.java
@@ -0,0 +1,13 @@
+package week11_juan_117229_tutorial;
+
+public class CustomizeException {
+
+    public static void main(String[] args) {
+        try {
+            int[] arr = new int[4];
+            int i = arr[4];
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
